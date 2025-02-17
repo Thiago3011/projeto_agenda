abe4a5212624c0aff447f8c1364b64780f42e05b
Thiago3011 <thiago.silva1001@outlook.com>
2025-02-17 20:59:38 -0300

    função de edição realizada.

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ def adicionar_contato(lista_contatos, nome, telefone, email):
     return
 
 def visualizar_contatos(lista_contatos):
-    for indice, contato in enumerate(lista_contatos):
+    for indice, contato in enumerate(lista_contatos, start=1):
         status = "★" if contato["favorito"] else "☆"
         nome_contato = contato["nome"]
         telefone_contato = contato["telefone"]
         email_contato = contato["email"]
         print(f"\n{indice}. [ {status} ] - {nome_contato} ({email_contato}) -> {telefone_contato}")
+    return
+
+def editar_contato(lista_contatos, contato_para_alteracao, nome_alterado, telefone_alterado, email_alterado, favorito):
+    indice_arrumado = int(contato_para_alteracao) - 1
+    nome = lista_contatos[indice_arrumado]["nome"] = nome_alterado if nome_alterado != "" else lista_contatos[indice_arrumado]["nome"]
+    telefone = lista_contatos[indice_arrumado]["telefone"] = telefone_alterado if telefone_alterado != "" else lista_contatos[indice_arrumado]["telefone"]
+    email = lista_contatos[indice_arrumado]["email"] = email_alterado if email_alterado != "" else lista_contatos[indice_arrumado]["email"]
+    status = lista_contatos[indice_arrumado]["favorito"] = "★" if favorito == "S" or "s" else "☆"
+    
+    print(f"""\nO contato foi alterado com sucesso:
+          
+    {indice_arrumado + 1}. [ {status} ] - {nome} ({email}) -> {telefone}""")
     return
 
 lista_contatos = []
@@ -33,5 +45,14 @@ while True:
         adicionar_contato(lista_contatos, nome, telefone, email)
     if escolha == "2":
         visualizar_contatos(lista_contatos)
+    if escolha == "3":
+        visualizar_contatos(lista_contatos)
+        contato_para_alteracao = input("\nPor favor, digite o número do contato a ser editado: ")
+        print("\nAbaixo, seguem os itens para alteração, e caso não deseje alterar, deixe o campo vazio e aperte 'enter'")
+        nome = input("Novo nome: ")
+        telefone = input("Novo telefone: ")
+        email = input("Nome email: ")
+        favorito = input("Gostaria de incluir o contato como favorito (S = Sim ou N = Nao)?: ")
+        editar_contato(lista_contatos, contato_para_alteracao, nome, telefone, email, favorito)
     if escolha == "6":
         break
