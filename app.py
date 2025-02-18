7649a16c43bc3983e8b590d389d8f73240654120
Thiago3011 <thiago.silva1001@outlook.com>
2025-02-17 21:41:47 -0300

    programa finalizado

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def adicionar_contato(lista_contatos, nome, telefone, email):
 
 def visualizar_contatos(lista_contatos):
     for indice, contato in enumerate(lista_contatos, start=1):
-        status = "★" if contato["favorito"] else "☆"
+        status = "★" if contato["favorito"] else ""
         nome_contato = contato["nome"]
         telefone_contato = contato["telefone"]
         email_contato = contato["email"]
@@ -18,11 +18,27 @@ def editar_contato(lista_contatos, contato_para_alteracao, nome_alterado, telefo
     nome = lista_contatos[indice_arrumado]["nome"] = nome_alterado if nome_alterado != "" else lista_contatos[indice_arrumado]["nome"]
     telefone = lista_contatos[indice_arrumado]["telefone"] = telefone_alterado if telefone_alterado != "" else lista_contatos[indice_arrumado]["telefone"]
     email = lista_contatos[indice_arrumado]["email"] = email_alterado if email_alterado != "" else lista_contatos[indice_arrumado]["email"]
-    status = lista_contatos[indice_arrumado]["favorito"] = "★" if favorito == "S" or "s" else "☆"
+    status = lista_contatos[indice_arrumado]["favorito"] = "★" if favorito == "S" or "s" else ""
     
     print(f"""\nO contato foi alterado com sucesso:
           
-    {indice_arrumado + 1}. [ {status} ] - {nome} ({email}) -> {telefone}""")
+    {indice_arrumado + 1}. [{status}] - {nome} ({email}) -> {telefone}""")
+    return
+
+def visualizar_contatos_favoritos(lista_contatos):
+    if len(lista_contatos) == 0: return print("\nNão há contatos favoritados. Para favoritar, volte ao menu e escolha a opção de edição.")
+
+    for indice, contato in enumerate(lista_contatos, start=1):
+        if contato["favorito"] == "★":
+            nome_contato = contato["nome"]
+            telefone_contato = contato["telefone"]
+            email_contato = contato["email"]
+            print(f"\n{indice}. [ ★ ] - {nome_contato} ({email_contato}) -> {telefone_contato}")
+
+def deletar_contato(lista_contatos, contato_para_delecao):
+    indice_contato_arrumado = int(contato_para_delecao) - 1
+    lista_contatos.remove(lista_contatos[indice_contato_arrumado])
+    print("O contato foi deletado com sucesso!")
     return
 
 lista_contatos = []
@@ -54,5 +70,11 @@ while True:
         email = input("Nome email: ")
         favorito = input("Gostaria de incluir o contato como favorito (S = Sim ou N = Nao)?: ")
         editar_contato(lista_contatos, contato_para_alteracao, nome, telefone, email, favorito)
+    if escolha == "4":
+        visualizar_contatos_favoritos(lista_contatos)
+    if escolha == "5":
+        visualizar_contatos(lista_contatos)
+        contato_para_delecao = input("\nPor favor, digite o número do contato a ser deletado: ")
+        deletar_contato(lista_contatos, contato_para_delecao)
     if escolha == "6":
         break
