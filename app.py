b2e6abe3f8c4350f9860c08426bb128fcb15a19f
Thiago3011 <thiago.silva1001@outlook.com>
2025-02-17 22:16:11 -0300

    tratamento de exceções

--- app.py
+++ app.py
@@ -1,45 +1,81 @@
 def adicionar_contato(lista_contatos, nome, telefone, email):
-    contato = {"nome": nome, "telefone": telefone, "email": email, "favorito": False}
-    lista_contatos.append(contato)
-    print(f"\nO contato {contato["nome"]} foi adicionado com sucesso!")
-    return
+    try:
+        if not nome or not telefone or not email:
+            raise ValueError("\nPor favor, é necessário preencher todos os campos!")
+        else:
+            contato = {"nome": nome, "telefone": telefone, "email": email, "favorito": False}
+    except Exception as e:
+        print(f"\nErro ao realizar a operação de inclusão do novo contato: {e}")
+    else:
+        lista_contatos.append(contato)
+        print(f"\nO contato '{contato["nome"]}' foi adicionado com sucesso!")
 
 def visualizar_contatos(lista_contatos):
-    for indice, contato in enumerate(lista_contatos, start=1):
-        status = "★" if contato["favorito"] else ""
-        nome_contato = contato["nome"]
-        telefone_contato = contato["telefone"]
-        email_contato = contato["email"]
-        print(f"\n{indice}. [ {status} ] - {nome_contato} ({email_contato}) -> {telefone_contato}")
-    return
+    try:
+        if not lista_contatos:
+            raise ValueError("\nNão há contatos cadastrados!")           
+        else:
+            for indice, contato in enumerate(lista_contatos, start=1):
+                status = "★" if contato["favorito"] else ""
+                nome_contato = contato["nome"]
+                telefone_contato = contato["telefone"]
+                email_contato = contato["email"]
+                print(f"\n{indice}. [ {status} ] - {nome_contato} ({email_contato}) -> {telefone_contato}")
+    except Exception as e:
+        print(f"\nErro ao realizar a operação: {e}")
 
 def editar_contato(lista_contatos, contato_para_alteracao, nome_alterado, telefone_alterado, email_alterado, favorito):
-    indice_arrumado = int(contato_para_alteracao) - 1
-    nome = lista_contatos[indice_arrumado]["nome"] = nome_alterado if nome_alterado != "" else lista_contatos[indice_arrumado]["nome"]
-    telefone = lista_contatos[indice_arrumado]["telefone"] = telefone_alterado if telefone_alterado != "" else lista_contatos[indice_arrumado]["telefone"]
-    email = lista_contatos[indice_arrumado]["email"] = email_alterado if email_alterado != "" else lista_contatos[indice_arrumado]["email"]
-    status = lista_contatos[indice_arrumado]["favorito"] = "★" if favorito == "S" or "s" else ""
-    
-    print(f"""\nO contato foi alterado com sucesso:
-          
-    {indice_arrumado + 1}. [{status}] - {nome} ({email}) -> {telefone}""")
-    return
-
+    try:
+        if not contato_para_alteracao:
+            raise ValueError("\nPor favor, digite um número de acordo com a lista!") 
+        else:          
+            indice_arrumado = int(contato_para_alteracao) - 1
+            nome = lista_contatos[indice_arrumado]["nome"] = nome_alterado if nome_alterado != "" else lista_contatos[indice_arrumado]["nome"]
+            telefone = lista_contatos[indice_arrumado]["telefone"] = telefone_alterado if telefone_alterado != "" else lista_contatos[indice_arrumado]["telefone"]
+            email = lista_contatos[indice_arrumado]["email"] = email_alterado if email_alterado != "" else lista_contatos[indice_arrumado]["email"]
+            status = lista_contatos[indice_arrumado]["favorito"] = "★" if favorito == "S" or favorito == "s" else ""
+    except Exception as e:
+        print(f"\nErro ao realizar a operação de alteração do contato: {e}")
+    else:
+        print(f"""\nO contato foi alterado com sucesso:
+            
+        {indice_arrumado + 1}. [{status}] - {nome} ({email}) -> {telefone}""")
+        
 def visualizar_contatos_favoritos(lista_contatos):
-    if len(lista_contatos) == 0: return print("\nNão há contatos favoritados. Para favoritar, volte ao menu e escolha a opção de edição.")
-
-    for indice, contato in enumerate(lista_contatos, start=1):
-        if contato["favorito"] == "★":
-            nome_contato = contato["nome"]
-            telefone_contato = contato["telefone"]
-            email_contato = contato["email"]
-            print(f"\n{indice}. [ ★ ] - {nome_contato} ({email_contato}) -> {telefone_contato}")
+    try:
+        if not lista_contatos:
+            raise ValueError("\nNão há contatos cadastrados!")           
+        else:
+            for indice, contato in enumerate(lista_contatos, start=1):
+                if contato["favorito"] == "★":
+                    nome_contato = contato["nome"]
+                    telefone_contato = contato["telefone"]
+                    email_contato = contato["email"]
+                else:
+                    raise ValueError("\nNão há contatos favoritados!")                        
+    except Exception as e:
+        print(f"\nErro ao realizar a operação de visualização da lista de favoritos: {e}")
+    else:
+        for indice, contato in enumerate(lista_contatos, start=1):
+            if contato["favorito"] == "★":
+                nome_contato = contato["nome"]
+                telefone_contato = contato["telefone"]
+                email_contato = contato["email"]
+                print(f"\n{indice}. [ ★ ] - {nome_contato} ({email_contato}) -> {telefone_contato}")
 
 def deletar_contato(lista_contatos, contato_para_delecao):
-    indice_contato_arrumado = int(contato_para_delecao) - 1
-    lista_contatos.remove(lista_contatos[indice_contato_arrumado])
-    print("O contato foi deletado com sucesso!")
-    return
+    try:
+        if not contato_para_delecao:
+            raise ValueError("\nPor favor, é necessário informar o contato para deleção!")
+        elif not lista_contatos:
+            raise ValueError("\nNão há contatos para serem deletados!")
+        else:
+            indice_contato_arrumado = int(contato_para_delecao) - 1
+    except Exception as e:
+        print(f"\nErro ao realizar a operação de inclusão do novo contato: {e}")
+    else:
+        lista_contatos.remove(lista_contatos[indice_contato_arrumado])
+        print("O contato foi deletado com sucesso!")
 
 lista_contatos = []
 
@@ -62,14 +98,17 @@ while True:
     if escolha == "2":
         visualizar_contatos(lista_contatos)
     if escolha == "3":
-        visualizar_contatos(lista_contatos)
-        contato_para_alteracao = input("\nPor favor, digite o número do contato a ser editado: ")
-        print("\nAbaixo, seguem os itens para alteração, e caso não deseje alterar, deixe o campo vazio e aperte 'enter'")
-        nome = input("Novo nome: ")
-        telefone = input("Novo telefone: ")
-        email = input("Nome email: ")
-        favorito = input("Gostaria de incluir o contato como favorito (S = Sim ou N = Nao)?: ")
-        editar_contato(lista_contatos, contato_para_alteracao, nome, telefone, email, favorito)
+        if not lista_contatos:
+            print("\nNão há contatos cadastrados para edição!")
+        else:
+            visualizar_contatos(lista_contatos)
+            contato_para_alteracao = input("\nPor favor, digite o número do contato a ser editado: ")
+            print("\nAbaixo, seguem os itens para alteração, e caso não deseje alterar, deixe o campo vazio e aperte 'enter'")
+            nome = input("Novo nome: ")
+            telefone = input("Novo telefone: ")
+            email = input("Nome email: ")
+            favorito = input("Gostaria de incluir o contato como favorito (S = Sim ou N = Nao)?: ")
+            editar_contato(lista_contatos, contato_para_alteracao, nome, telefone, email, favorito)
     if escolha == "4":
         visualizar_contatos_favoritos(lista_contatos)
     if escolha == "5":
